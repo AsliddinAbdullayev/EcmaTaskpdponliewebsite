48b0daae6f11b0369d6f822fab757fe09fb56dbd
AsliddinAbdullayev <asliddinfrontweb2006@gmail.com>
2021-04-11 13:39:55 +0500

    AsliddinEcmaTask

---
+++ src/pages/ThreePage.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class ThreePage extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default ThreePage;
